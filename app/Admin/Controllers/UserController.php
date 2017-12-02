5bdcd232921077140f6f16ad8f3a772ee92e7fdd
rachel <hai30@163.com>
2017-12-02 12:39:16 +0800

    first commit

---
+++ app/Admin/Controllers/UserController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\User;
+
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Layout\Content;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\ModelForm;
+
+class UserController extends Controller
+{
+    use ModelForm;
+
+    /**
+     * Index interface.
+     *
+     * @return Content
+     */
+    public function index()
+    {
+        return Admin::content(function (Content $content) {
+
+            $content->header('header');
+            $content->description('description');
+
+            $content->body($this->grid());
+        });
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param $id
+     * @return Content
+     */
+    public function edit($id)
+    {
+        return Admin::content(function (Content $content) use ($id) {
+
+            $content->header('header');
+            $content->description('description');
+
+            $content->body($this->form()->edit($id));
+        });
+    }
+
+    /**
+     * Create interface.
+     *
+     * @return Content
+     */
+    public function create()
+    {
+        return Admin::content(function (Content $content) {
+
+            $content->header('header');
+            $content->description('description');
+
+            $content->body($this->form());
+        });
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        return Admin::grid(User::class, function (Grid $grid) {
+
+            $grid->id('ID')->sortable();
+
+            $grid->column('name','用户名称')->sortable();
+            $grid->column('email','E-mail');
+
+            $grid->filter(function ($filter) {
+                $filter->equal('name','名称');
+
+            });
+
+            $grid->created_at();
+            $grid->updated_at();
+        });
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        return Admin::form(User::class, function (Form $form) {
+
+            $form->display('id', 'ID');
+            $form->text('name', '用户名称');
+            $form->text('email', '邮箱');
+            $form->text('password', 'pass');
+
+            $form->display('created_at', 'Created At');
+            $form->display('updated_at', 'Updated At');
+        });
+    }
+}
